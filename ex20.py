dc54f40e781abe1b588ab9d4de65a8e2e51ae2c3
leonfliu@gmail.com <leonfliu@gmail.com>
2015-04-14 18:02:02 -0500

    functions can return something

--- ex20.py
+++ ex20.py
@@ -21,14 +21,16 @@ print_all(current_file)
 print "Now let's rewind, kind of like a tape."
 
 rewind(current_file)
-
+## if not rewind file, i.e. current_file(0), set the pointer at the file to the begining of the file, it will at the end of the file. then when try to print the three lines, as following, it will not give any result.
 print "Let's print three lines:"
 
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += current_line
+#current_line = current_line + 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += current_line
+#current_line = current_line + 1
 print_a_line(current_line, current_file)
