87945346d5505010621b22470da37e3fff2baa07
leonfliu@gmail.com <leonfliu@gmail.com>
2015-04-13 19:20:35 -0500

    example 13

---
+++ ex13.py
@@ -0,0 +1,10 @@
+# parameters, unpacking,variables
+from sys import argv
+# way to run this script: python ex13.py first 2nd 3rd
+script, first, second, third = argv
+
+print "THe script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
